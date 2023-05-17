4f375c57bf41a24f16d473dfb6702325ddff2f25
Shiv Bhonde <shivbhonde04@gmail.com>
2023-05-17 17:48:53 +0530

    add basic comments in deploy script

--- packages/hardhat/deploy/00_deploy_yourContract.ts
+++ packages/hardhat/deploy/00_deploy_yourContract.ts
@@ -27,24 +27,23 @@ const deployYourContractContract: DeployFunction = async function (hre: HardhatR
   // Get contract factory for YourContract
   const YourContract = await hre.ethers.getContractFactory("YourContract");
 
-  // "0xF37dB18dA71970FeE6242EdFF9cb5C841D68F117" is my burner(frontend) address
+  // "0xF37dB18dA71970FeE6242EdFF9cb5C841D68F117" is my burner(frontend) address, add yours (This will be passed to initialze function)
   const proxy = (await hre.upgrades.deployProxy(YourContract, ["0xF37dB18dA71970FeE6242EdFF9cb5C841D68F117"], {
     kind: "uups",
   })) as YourContract;
 
-  // Wait for 3 blocks to confirm the transaction if we are on a live network.
+  // Wait for 3 blocks to confirm the transaction if we are on a live network
   if (chainId !== "31337") {
     log("Waiting for 2 blocks to be mined...");
     await proxy.deployTransaction.wait(2);
     log("Transaction confirmed.");
   }
-
+  // We had to wait for blocks to be mined before we could get the implementation address because sometimes its error out
   const implementationAddress = await hre.upgrades.erc1967.getImplementationAddress(proxy.address);
 
   log(`YourContract Proxy deployed to: ${proxy.address}`);
   log(`YourContract Implementation deployed to: ${implementationAddress}`);
 
-  log(`YourContract Proxy ownership transferred to: 0xF37dB18dA71970FeE6242EdFF9cb5C841D68F117`);
   const artifacts = await hre.deployments.getExtendedArtifact("YourContract");
 
   const proxyDeployment = {
